f18badfb41ff8a3c50c6f5c53fcb5367340e5f41
Odissey7 <agapidu@inbox.ru>
2023-06-25 13:01:03 +0300

    Отправляет на проверку 3дз

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,17 @@
-const line = function (words, length) {
-  console.log(words.length <= length);
-}
+// const stringLength = (string, length) => {
+//   console.log(string.length <= length);
+// };
 
-line('programming', 8);
+// // Тест:
+// // Строка длиннее
+// stringLength('some string', 5);
+// // Строка короче
+// stringLength('some string', 22);
+// stringLength('js', 6);
+// // Строка равна
+// stringLength('any string', 10);
 
-line('hard', 13);
+// const stringLength2 = (string, length) => string.length <= length;
 
-line('everything is easy', 19);
-
-line('learning', 4);
+let numder = Math.round(Math.random() * 100);
+console.log(numder);
