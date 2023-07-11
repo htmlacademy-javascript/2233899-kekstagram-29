290ce8611345eaf0a3a2b68d8e2796f2d0170970
Odissey7 <agapidu@inbox.ru>
2023-07-11 19:00:57 +0300

    Отправляет на проверку 5дз2

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import {getRandomNumder} from "./util.js";
-import {getUniqueNumber} from "./util.js";
-import {random} from "./util.js";
-console.log(random)
-console.log(getRandomNumder)
-console.log(getUniqueNumber)
